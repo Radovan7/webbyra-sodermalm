0549ad22fb682d17fb8a4b8318929d34795c9975
Radovan <radosce0@gmail.com>
2024-02-22 22:10:06 +0100

    SP-3/Create HeroSection
    Created hero section implementing design from figma
    
    SP-3

--- components/Hero.js
+++ components/Hero.js
@@ -1,29 +1,22 @@
-import Image from "next/image";
-
 const content = {
-  title: "Code Crafter Starter Template",
-  subtitle: "A badass template based on Bulma CSS framework.",
-  heroLogo: "/android-chrome-512x512.png",
+  title: "LÅT OSS BYGGA DIN WEBBLÖSNING!",
   heroClass: "is-medium is-black",
-}
+  ctaButton: { text: "Kontakta oss", link: "/" },
+};
 
 export default function Hero() {
   return (
     <section className={`hero ${content.heroClass}`}>
       <div className="container">
         <div className="hero-body">
-          {content.heroLogo && (
-            <Image
-              src={content.heroLogo}
-              alt="Hero logo"
-              width={120}
-              height={68}
-            />
-          )}
-          <p className="title pb-4 pt-4 has-text-centered-mobile">{content.title}</p>
-          <p className="subtitle has-text-centered-mobile">
-            {content.subtitle}
+          <p className="title pb-4 pt-4 has-text-centered-mobile">
+            {content.title}
           </p>
+          {content.ctaButton && (
+            <button className="button" to={content.ctaButton.link}>
+              {content.ctaButton.text}
+            </button>
+          )}
         </div>
       </div>
     </section>
