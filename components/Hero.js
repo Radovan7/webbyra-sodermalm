683f4febac24fa483bb07d78847d018f1608adfe
Radovan <radosce0@gmail.com>
2024-02-26 22:26:19 +0100

    Implemented styles to resamble figma design

--- components/Hero.js
+++ components/Hero.js
@@ -1,19 +1,19 @@
 const content = {
   title: "LÅT OSS BYGGA DIN WEBBLÖSNING!",
   heroClass: "is-medium is-black",
-  ctaButton: { text: "Kontakta oss", link: "/" },
+  ctaButton: { text: "kontakta oss", link: "/" },
 };
 
 export default function Hero() {
   return (
-    <section className={`hero ${content.heroClass}`}>
+    <section className={`hero ${content.heroClass} is-flex `}>
       <div className="container">
-        <div className="hero-body">
-          <p className="title pb-4 pt-4 has-text-centered-mobile">
+        <div className="hero-body is-flex is-justify-content-center is-align-items-center is-flex-direction-column">
+          <p className="title title-styles">
             {content.title}
           </p>
           {content.ctaButton && (
-            <button className="button" to={content.ctaButton.link}>
+            <button className="button button-styles" to={content.ctaButton.link} >
               {content.ctaButton.text}
             </button>
           )}
