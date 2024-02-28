56ba1779717f8fe948b6b19de84c7e63ac15d948
Blazze23 <l.azmar23@gmail.com>
2024-02-28 21:06:28 +0100

    SP-5/Create text section
    Created text section implementing the styles from figma
    
    SP-5

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,31 @@
-'use client'
+"use client";
 
-import Hero from '../components/Hero'
-import Cards from '../components/Cards'
-import ContentBlock from '../components/ContentBlock'
-import Carousel from '../components/Carousel'
-import CTA from '../components/CTA'
+import Hero from "../components/Hero";
+import Cards from "../components/Cards";
+import Carousel from "../components/Carousel";
+import CTA from "../components/CTA";
+import ContentBlock from "../components/ContentBlock";
+
+const content = {
+  title: "",
+  subtitle:
+    "• Mobilanpassning • Responsive • Hemsida i Wordpress • E-handel • Woocommerce • Appar • E-postkommunikation • Webbhotell • Egen server • Konsulttjänster • Hackad hemsida",
+  description:
+    "Vi är en webbyrå i Stockholm som kan hjälpa med att skapa hela lösningen till dig och din affär. Vi bygger en säljande webbsida åt dig, lägga webbsida på säkert SERVER/WEBBHOTELL, skapa afärs E-POSTADRESS och göra bästa möjliga google optimmerade sida med bra SEO ranking! Har din hemsida blivit HACKAD eller behöver uppdatering? Ingen problem, vi kan hjälpa...",
+  link: {
+    to: "/las-mer",
+    text: "LÄS MER",
+  },
+};
 
 export default function Index() {
   return (
     <>
       <Hero />
       <Cards />
-      <ContentBlock />
+      <ContentBlock content={content} />
       <Carousel />
       <CTA />
     </>
-  )
+  );
 }
