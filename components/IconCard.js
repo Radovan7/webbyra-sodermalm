86cc4f8127fc3b69367666e0025958e1fe3b0a57
Blazze23 <l.azmar23@gmail.com>
2024-03-19 20:45:05 +0100

    SP-12/Sync with figma
    Improve hover animation on portfolio section for better UX
    
    SP-12

--- components/IconCard.js
+++ components/IconCard.js
@@ -1,20 +1,23 @@
 import Image from "next/image"
 
-export default function IconCard({ title, icon, tag, bgColor }) {
+export default function IconCard({ title, icon, tag, bgColor, url }) {
   return (
-    <div className="separator">
-      <div className="social-card-styles has-text-centered" style={{ backgroundColor: bgColor }}>
-        <div className="mt-5">
-          <Image
-            src={icon.url}
-            alt={icon.alt}
-          />
+    <a href={url} target="_blank">
+      <div className="separator">
+        <div className="social-card-styles has-text-centered" style={{ backgroundColor: bgColor }}>
+          <div className="mt-5">
+            <Image
+              src={icon.url}
+              alt={icon.alt}
+            />
+          </div>
+          <div className="pb-6 pt-3">
+            <h3 className="is-size-3 has-text-weight-bold">{title}</h3>
+            <small>{tag}</small>
+          </div>
         </div>
-        <div className="pb-6 pt-3">
-          <h3 className="is-size-3 has-text-weight-bold">{title}</h3>
-          <small>{tag}</small>
-        </div>
-      </div>
-    </div >
+      </div >
+    </a>
   )
 }
+
